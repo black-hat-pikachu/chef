7a7ceeb2a1ba02816b80a6fb43cb9482fd9557f2
Lamont Granquist <lamont@scriptkiddie.org>
2021-03-09 15:42:28 -0800

    only run one crontab spec
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -151,4 +151,5 @@ export CHEF_LICENSE=accept-no-persist
 
 cd "$chef_gem"
 sudo -E bundle install --jobs=3 --retry=3
-sudo -E bundle exec rspec --profile -f progress
+sudo -E bundle exec rspec ./spec/functional/resource/cron_spec.rb:82 
+#sudo -E bundle exec rspec --profile -f progress
